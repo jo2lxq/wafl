e3d8de206d5253ce08118954b53763ebb24e6503
Ryuhei Yamaguchi <yama20001022@g.ecc.u-tokyo.ac.jp>
2025-02-11 17:14:24 +0900

    set weights_only=False in torch.load

--- WAFL-YOLO/src/models/experimental.py
+++ WAFL-YOLO/src/models/experimental.py
@@ -238,7 +238,7 @@ def attempt_load(weights, device=None, inplace=True, fuse=True):
 
     model = Ensemble()
     for w in weights if isinstance(weights, list) else [weights]:
-        ckpt = torch.load(attempt_download(w), map_location='cpu')  # load
+        ckpt = torch.load(attempt_download(w), weights_only=False, map_location='cpu')  # load
         ckpt = (ckpt.get('ema') or ckpt['model']).to(device).float()  # FP32 model
 
         # Model compatibility updates
