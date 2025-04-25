23eaa205457617409b628ee92fd7aeb7b7005800
yagiryo0219 <yagi-ryo143@g.ecc.u-tokyo.ac.jp>
2025-04-25 20:41:32 +0900

    関数名変更の対応

--- WAFL-Whisper/src/__init__.py
+++ WAFL-Whisper/src/__init__.py
@@ -1,7 +1,7 @@
 from src.config.config_manager import ConfigManager
 from src.data import AudioScriptDataset, WhisperDataCollatorWhithPadding, prepare_datasets, prepare_test_dataset
 from src.model import exchange_parameter_with_close_nodes, save_model
-from src.utils import eval_cer_of_each_node, train_each_model
+from src.utils import eval_cer_of_all_node, train_all_model
 
 __all__ = [
     "ConfigManager",
@@ -11,6 +11,6 @@ __all__ = [
     "prepare_test_dataset",
     "exchange_parameter_with_close_nodes",
     "save_model",
-    "eval_cer_of_each_node",
-    "train_each_model",
+    "eval_cer_of_all_node",
+    "train_all_model",
 ]
--- WAFL-Whisper/src/utils/__init__.py
+++ WAFL-Whisper/src/utils/__init__.py
@@ -1,7 +1,7 @@
-from src.utils.eval import eval_cer_of_each_node
-from src.utils.train import train_each_model
+from src.utils.eval import eval_cer_of_all_node
+from src.utils.train import train_all_model
 
 __all__ = [
-    "eval_cer_of_each_node",
-    "train_each_model",
+    "eval_cer_of_all_node",
+    "train_all_model",
 ]
