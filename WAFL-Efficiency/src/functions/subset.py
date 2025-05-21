cb31207c02ca4774dfe54ff8a47f098bd84d6827
KT220m <tsuchiya-kaito220@g.ecc.u-tokyo.ac.jp>
2025-05-21 11:47:14 +0900

    Modify indent

--- WAFL-Efficiency/src/functions/subset.py
+++ WAFL-Efficiency/src/functions/subset.py
@@ -6,30 +6,30 @@ from torchvision import transforms
 
 def create_subsets(dataset, num_nodes, class_to_node, bias_ratio=0.9, seed=1):
 
-    random.seed(seed)
-    class_indices = defaultdict(list)
+	random.seed(seed)
+	class_indices = defaultdict(list)
 
-    for idx, (_, label) in enumerate(dataset):
-        class_indices[label].append(idx)
+	for idx, (_, label) in enumerate(dataset):
+		class_indices[label].append(idx)
 
-    for cls in class_indices:
-        random.shuffle(class_indices[cls])
+	for cls in class_indices:
+		random.shuffle(class_indices[cls])
 
-    node_indices = [[] for _ in range(num_nodes)]
+	node_indices = [[] for _ in range(num_nodes)]
 
-    for cls, indices in class_indices.items():
-        main_node = class_to_node[cls]
-        total = len(indices)
-        bias_count = int(total * bias_ratio)
+	for cls, indices in class_indices.items():
+		main_node = class_to_node[cls]
+		total = len(indices)
+		bias_count = int(total * bias_ratio)
 
-        node_indices[main_node].extend(indices[:bias_count])
+		node_indices[main_node].extend(indices[:bias_count])
 
-        rest = indices[bias_count:]
-        other_nodes = [i for i in range(num_nodes) if i != main_node]
-        for i, idx in enumerate(rest):
-            node = other_nodes[i % len(other_nodes)]
-            node_indices[node].append(idx)
+		rest = indices[bias_count:]
+		other_nodes = [i for i in range(num_nodes) if i != main_node]
+		for i, idx in enumerate(rest):
+			node = other_nodes[i % len(other_nodes)]
+			node_indices[node].append(idx)
 
-    subsets = [Subset(dataset, sorted(idxs)) for idxs in node_indices]
-    return subsets
+	subsets = [Subset(dataset, sorted(idxs)) for idxs in node_indices]
+	return subsets
 
--- WAFL-Efficiency/src/mnist.py
+++ WAFL-Efficiency/src/mnist.py
@@ -53,20 +53,20 @@ random.seed(1)
 # directory for storing the prev_net data
 tmp_dir = "./tmp" 
 if os.path.isdir(tmp_dir):
-    for filename in os.listdir(tmp_dir):
-        os.remove(tmp_dir + filename)
+	for filename in os.listdir(tmp_dir):
+		os.remove(tmp_dir + filename)
 else:
-    os.makedirs(tmp_dir)
+	os.makedirs(tmp_dir)
 save_dir = "../trained_net" 
 if not os.path.isdir(tmp_dir):
-    os.makedirs(save_dir)
+	os.makedirs(save_dir)
 
 # prepare dataloader
 train_transform = transforms.Compose([
-    transforms.ToTensor(),
+	transforms.ToTensor(),
 ])
 val_transform = transforms.Compose([
-    transforms.ToTensor(),
+	transforms.ToTensor(),
 ])
 
 trainset = torchvision.datasets.MNIST(root='../data', train = True, download=True, transform=train_transform)
@@ -75,19 +75,19 @@ trainloaders = []
 print("Data loaded")
 
 if noniid == 0: # iid
-    trainset_size = int(len(trainset) / node_num)
-    for n in range(node_num):
-        indices = list(range(n * trainset_size, (n + 1) * trainset_size))
-        traindataset = Subset(trainset, indices)
-        trainloaders.append(torch.utils.data.DataLoader(traindataset, batch_size = batch_size, shuffle = True))
+	trainset_size = int(len(trainset) / node_num)
+	for n in range(node_num):
+		indices = list(range(n * trainset_size, (n + 1) * trainset_size))
+		traindataset = Subset(trainset, indices)
+		trainloaders.append(torch.utils.data.DataLoader(traindataset, batch_size = batch_size, shuffle = True))
 elif 0 < noniid and noniid <= 1: # noniid
-    class_to_node = {cls: random.randint(0, node_num - 1) for cls in range(class_num)}
-    subsets = create_subsets(trainset, num_nodes = node_num , class_to_node = class_to_node, bias_ratio = noniid)
-    for n in range(node_num):
-        trainloaders.append(torch.utils.data.DataLoader(subsets[n], batch_size = batch_size, shuffle = True))
+	class_to_node = {cls: random.randint(0, node_num - 1) for cls in range(class_num)}
+	subsets = create_subsets(trainset, num_nodes = node_num , class_to_node = class_to_node, bias_ratio = noniid)
+	for n in range(node_num):
+		trainloaders.append(torch.utils.data.DataLoader(subsets[n], batch_size = batch_size, shuffle = True))
 else:
-    print(f"argument error : noniid = {noniid}")
-    sys.exit()
+	print(f"argument error : noniid = {noniid}")
+	sys.exit()
 
 valset = torchvision.datasets.MNIST(root='../data', train = False, download=True, transform=val_transform)
 valloader = torch.utils.data.DataLoader(valset, batch_size = batch_size, shuffle=True)
@@ -96,33 +96,33 @@ del trainset
 del valset
 
 class SimpleCNN(nn.Module):
-    def __init__(self):
-        super(SimpleCNN, self).__init__()
-        self.net = nn.Sequential(
-            nn.Conv2d(1, 16, kernel_size=3, padding=1),  
-            nn.ReLU(),
-            nn.MaxPool2d(2),                            
-            nn.Conv2d(16, 32, kernel_size=3, padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(2),       
-            nn.Flatten(),         
-            nn.Linear(32*7*7, 128),
-            nn.ReLU(),
-            nn.Linear(128, 10)                           
-        )
-    def forward(self, x):
-        return self.net(x)
+	def __init__(self):
+		super(SimpleCNN, self).__init__()
+		self.net = nn.Sequential(
+			nn.Conv2d(1, 16, kernel_size=3, padding=1),  
+			nn.ReLU(),
+			nn.MaxPool2d(2),							
+			nn.Conv2d(16, 32, kernel_size=3, padding=1),
+			nn.ReLU(),
+			nn.MaxPool2d(2),	   
+			nn.Flatten(),		  
+			nn.Linear(32*7*7, 128),
+			nn.ReLU(),
+			nn.Linear(128, 10)							 
+		)
+	def forward(self, x):
+		return self.net(x)
 
 nets = [SimpleCNN() for n in range(node_num)]
 
 prev_net = [[None] * node_num for _ in range(node_num)]
 init_net = None
 if zero_initialization == True: # initializa the prev_nets with zero
-    init_net = copy.deepcopy(nets[0].state_dict())
-    for key in init_net.keys():
-        init_net[key][:] = 0
+	init_net = copy.deepcopy(nets[0].state_dict())
+	for key in init_net.keys():
+		init_net[key][:] = 0
 for net in nets:
-    net.to(device)
+	net.to(device)
 
 print("Model loaded")
 
@@ -133,38 +133,38 @@ print("Start Pre-self Training")
 
 # pre_self training
 for epoch in range(pre_epoch):
-    for n in range(node_num):
-        net = nets[n]
-        optimizer = optimizers[n]
-        trainloader = trainloaders[n]
+	for n in range(node_num):
+		net = nets[n]
+		optimizer = optimizers[n]
+		trainloader = trainloaders[n]
 
-        net.train()
-        running_loss = 0.0
-        correct = 0
-        total = 0
+		net.train()
+		running_loss = 0.0
+		correct = 0
+		total = 0
 
-        for batch_index, (inputs, labels) in enumerate(trainloader):
-            if batch_index == steps_per_epoch:
-                break
-            inputs, labels = inputs.to(device), labels.to(device)
-            optimizer.zero_grad()
-            outputs = net(inputs)
-            loss = criterion(outputs, labels)
-            loss.backward()
-            optimizer.step()
-            running_loss += loss.item()
-            _, predicted = torch.max(outputs, 1)
-            total += labels.size(0)
-            correct += (predicted == labels).sum().item()
-        print(f"Epoch [{epoch+1}/{pre_epoch}] Node {n} Loss {running_loss/len(trainloader):.4f} Accuracy {100 * correct/total:.2f}%")
+		for batch_index, (inputs, labels) in enumerate(trainloader):
+			if batch_index == steps_per_epoch:
+				break
+			inputs, labels = inputs.to(device), labels.to(device)
+			optimizer.zero_grad()
+			outputs = net(inputs)
+			loss = criterion(outputs, labels)
+			loss.backward()
+			optimizer.step()
+			running_loss += loss.item()
+			_, predicted = torch.max(outputs, 1)
+			total += labels.size(0)
+			correct += (predicted == labels).sum().item()
+		print(f"Epoch [{epoch+1}/{pre_epoch}] Node {n} Loss {running_loss/len(trainloader):.4f} Accuracy {100 * correct/total:.2f}%")
 
 # save (after pre self traininig)
 for n in range(node_num):
-    torch.save(nets[n].state_dict(), f"{save_dir}/prenet_e{pre_epoch}_n{n}.pth")
+	torch.save(nets[n].state_dict(), f"{save_dir}/prenet_e{pre_epoch}_n{n}.pth")
 
 for n in range(node_num):
-    acc = evaluate(nets[n], valloader, device)
-    print(f"Node {n} Validation Accuracy {acc:.2f}%")
+	acc = evaluate(nets[n], valloader, device)
+	print(f"Node {n} Validation Accuracy {acc:.2f}%")
 
 print("Start WAFL")
 
@@ -174,54 +174,54 @@ contact_list = []
 filename=f'./contact_pattern/rwp_n10_a0500_r100_p10_s01.json'
 print(f'Loading ... {filename}')
 with open(filename) as f :
-    contact_list=json.load(f)
+	contact_list=json.load(f)
 
 for epoch in range(max_epoch):
-    contact = contact_list[epoch]
-    update_nets(nets, contact, fl_coefficient=1, reduce=reduce, K=K, Q=Q, zero_initialization = zero_initialization, prev_net=prev_net, init_net = init_net, device = device)
+	contact = contact_list[epoch]
+	update_nets(nets, contact, fl_coefficient=1, reduce=reduce, K=K, Q=Q, zero_initialization = zero_initialization, prev_net=prev_net, init_net = init_net, device = device)
 
-    for n in range(node_num):
-        nbr = contact[str(n)]
-        if len(nbr) == 0:
-            continue
+	for n in range(node_num):
+		nbr = contact[str(n)]
+		if len(nbr) == 0:
+			continue
 
-        net = nets[n]
-        optimizer = optimizers[n]
-        trainloader = trainloaders[n]
+		net = nets[n]
+		optimizer = optimizers[n]
+		trainloader = trainloaders[n]
 
-        net.train()
-        running_loss = 0.0
-        correct = 0
-        total = 0
+		net.train()
+		running_loss = 0.0
+		correct = 0
+		total = 0
 
-        for batch_index, (inputs, labels) in enumerate(trainloader):
-            if batch_index == steps_per_epoch:
-                break
-            inputs, labels = inputs.to(device), labels.to(device)
-            optimizer.zero_grad()
-            outputs = net(inputs)
-            loss = criterion(outputs, labels)
-            loss.backward()
-            optimizer.step()
-            running_loss += loss.item()
-            _, predicted = torch.max(outputs, 1)
-            total += labels.size(0)
-            correct += (predicted == labels).sum().item()
-        print(f"Epoch [{epoch+1}/{max_epoch}] Node {n} Loss {running_loss/len(trainloader):.4f} Accuracy {100 * correct/total:.2f}%")
+		for batch_index, (inputs, labels) in enumerate(trainloader):
+			if batch_index == steps_per_epoch:
+				break
+			inputs, labels = inputs.to(device), labels.to(device)
+			optimizer.zero_grad()
+			outputs = net(inputs)
+			loss = criterion(outputs, labels)
+			loss.backward()
+			optimizer.step()
+			running_loss += loss.item()
+			_, predicted = torch.max(outputs, 1)
+			total += labels.size(0)
+			correct += (predicted == labels).sum().item()
+		print(f"Epoch [{epoch+1}/{max_epoch}] Node {n} Loss {running_loss/len(trainloader):.4f} Accuracy {100 * correct/total:.2f}%")
 
-    # evaluation
-    sum = 0
-    for n in range(node_num):
-        acc = evaluate(nets[n], valloader, device)
-        print(f"Epoch [{epoch+1}/{max_epoch}] Node {n} Validation Accuracy {acc:.2f}%")
-        sum += acc
-    print(f"Epoch [{epoch+1}/{max_epoch}] Avg Validation Accuracy {sum / node_num:.2f}%")
+	# evaluation
+	sum = 0
+	for n in range(node_num):
+		acc = evaluate(nets[n], valloader, device)
+		print(f"Epoch [{epoch+1}/{max_epoch}] Node {n} Validation Accuracy {acc:.2f}%")
+		sum += acc
+	print(f"Epoch [{epoch+1}/{max_epoch}] Avg Validation Accuracy {sum / node_num:.2f}%")
 
-    # save
-    if (epoch + 1) == max_epoch:
-        for n in range(node_num):
-            torch.save(nets[n].state_dict(), f"{save_dir}/net_e{epoch+1}_n{n}.pth")
-        
+	# save
+	if (epoch + 1) == max_epoch:
+		for n in range(node_num):
+			torch.save(nets[n].state_dict(), f"{save_dir}/net_e{epoch+1}_n{n}.pth")
+		
 for filename in os.listdir(tmp_dir):
-    os.remove(file_path)
+	os.remove(file_path)
 print(f"time : {time.time() - start_time}")
