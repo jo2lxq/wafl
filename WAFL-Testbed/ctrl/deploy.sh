449310a8d16ffa67c2bbe9259d6d2727493ac90e
namitshah <namitvshah@gmail.com>
2025-06-19 11:17:29 +0900

    Updated the shell script and the base config file

--- WAFL-Testbed/ctrl/deploy.sh
+++ WAFL-Testbed/ctrl/deploy.sh
@@ -1,54 +1,53 @@
 #!/bin/bash
-
 # WAFL Testbed deployment shell script
-target_name=""
-target_location=""
-target_confirm=""
-# Enter the target directory name: 
-# Eg. WAFL_DEMO
-read -p "Please enter the target directory name: " target_name
-# Enter the target directory location: 
-# Eg. /home/workspace/namit/
-read -p "Please enter the target directory location: " target_location
-# Please confirm the full directory path: 
-# Eg. /home/workspace/namit/WAFL_DEMO
-read -p "Please confirm the full directory path: " target_confirm
-# The directory will be set on the execution servers as follows: /home/testbed/WAFL_DEMO
-if [ "$target_location$target_name" != "$target_confirm" ]
+
+# Please run the script from the PROJECT_DIR 
+# (not PROJECT_DIR/ctrl, for example)
+TARGET_PATH=$(pwd)
+TARGET_NAME=$(basename $TARGET_PATH)
+#Deserializing Base Configuration File Lists
+IFS=',' read -r -a WAFL_DEVICE_NAMES <<< "$WAFL_DEVICE_NAMES"
+IFS=',' read -r -a WAFL_DEVICE_IPS <<< "$WAFL_DEVICE_IPS"
+
+CONFIRM="DEFAULT"
+echo "The project directory to be deployed (PWD): $TARGET_PATH"
+read -p "Please enter 'DEPLOY' to confirm: " CONFIRM
+if [ "$CONFIRM" != "DEPLOY" ]
 then
-    echo "Directory names do not match!"
-    exit 1
-fi
-if [ ! -d "$target_confirm" ] 
-then
-    echo "Directory does not exist"
+    echo "Aborting the process"
     exit 1
 fi
 # Ensuring the existence of the base directories on the 
 # management server's project copy for replication
 # on the execution servers (from the wafl sub-directory)
-mkdir -p "$target_confirm/wafl/dataset"
-mkdir -p "$target_confirm/wafl/config"
-mkdir -p "$target_confirm/wafl/src"
-final_location="/home/denjo/testbed/"
+mkdir -p "$TARGET_PATH/wafl/dataset/common/train"
+mkdir -p "$TARGET_PATH/wafl/dataset/common/validate"
+mkdir -p "$TARGET_PATH/wafl/dataset/common/test"
+mkdir -p "$TARGET_PATH/wafl/config/common"
+mkdir -p "$TARGET_PATH/wafl/src/common"
 echo "Directory exists and will be replicated on all the execution servers via SSH"
-echo "Directories will have the following path: $final_location$target_name"
-
-# The Devices of the Network
-devices=("192.168.11.100" "192.168.11.101" "192.168.11.102" 
-        "192.168.11.103" "192.168.11.104" "192.168.11.105" 
-        "192.168.11.106" "192.168.11.107" "192.168.11.108" 
-        "192.168.11.109")
-user="denjo"
-for device in "${devices[@]}" 
+echo "Directories will have the following path: $DEPLOYMENT_LOCATION/$TARGET_NAME"
+for ((counter=0; counter<${#WAFL_DEVICE_NAMES[@]}; counter++))
 do
-    echo "Connecting to Execution Server: $device"
-    ssh "$user@$device" "rm -rf $final_location$target_name"
-    ssh "$user@$device" "mkdir -p $final_location$target_name"
-    scp -r -q "$target_confirm/wafl/dataset" "$user@$device:$final_location$target_name"
-    scp -r -q "$target_confirm/wafl/config" "$user@$device:$final_location$target_name"
-    scp -r -q "$target_confirm/wafl/src" "$user@$device:$final_location$target_name"
-    ssh "$user@$device" "mkdir -p $final_location$target_name/results"
+    # Ensuring the existence of the device-specific
+    # directories on the management server
+    mkdir -p "$TARGET_PATH/wafl/dataset/${WAFL_DEVICE_NAMES[$counter]}"
+    mkdir -p "$TARGET_PATH/wafl/config/${WAFL_DEVICE_NAMES[$counter]}"
+    mkdir -p "$TARGET_PATH/wafl/src/${WAFL_DEVICE_NAMES[$counter]}"
+    echo "Connecting to Execution Server: ${WAFL_DEVICE_NAMES[$counter]}"
+    ssh "$USER@${WAFL_DEVICE_IPS[$counter]}" "rm -rf $DEPLOYMENT_LOCATION/$TARGET_NAME; \
+        mkdir -p $DEPLOYMENT_LOCATION/$TARGET_NAME/dataset; \
+        mkdir -p $DEPLOYMENT_LOCATION/$TARGET_NAME/config;
+        mkdir -p $DEPLOYMENT_LOCATION/$TARGET_NAME/src;
+        mkdir -p $DEPLOYMENT_LOCATION/$TARGET_NAME/results"
+    # The Base Configuration shell script is also sent to the execution servers
+    scp -r -q "$TARGET_PATH/ctrl/wafl_base_execution_config" "$USER@${WAFL_DEVICE_IPS[$counter]}:$DEPLOYMENT_LOCATION/$TARGET_NAME"
+    scp -r -q "$TARGET_PATH/wafl/dataset/common/"* "$USER@${WAFL_DEVICE_IPS[$counter]}:$DEPLOYMENT_LOCATION/$TARGET_NAME/dataset" > /dev/null 2>&1
+    scp -r -q "$TARGET_PATH/wafl/config/common/"* "$USER@${WAFL_DEVICE_IPS[$counter]}:$DEPLOYMENT_LOCATION/$TARGET_NAME/config" > /dev/null 2>&1
+    scp -r -q "$TARGET_PATH/wafl/src/common/"* "$USER@${WAFL_DEVICE_IPS[$counter]}:$DEPLOYMENT_LOCATION/$TARGET_NAME/src" > /dev/null 2>&1
+    scp -r -q "$TARGET_PATH/wafl/dataset/${WAFL_DEVICE_NAMES[$counter]}/"* "$USER@${WAFL_DEVICE_IPS[$counter]}:$DEPLOYMENT_LOCATION/$TARGET_NAME/dataset" > /dev/null 2>&1
+    scp -r -q "$TARGET_PATH/wafl/config/${WAFL_DEVICE_NAMES[$counter]}/"* "$USER@${WAFL_DEVICE_IPS[$counter]}:$DEPLOYMENT_LOCATION/$TARGET_NAME/config" > /dev/null 2>&1
+    scp -r -q "$TARGET_PATH/wafl/src/${WAFL_DEVICE_NAMES[$counter]}/"* "$USER@${WAFL_DEVICE_IPS[$counter]}:$DEPLOYMENT_LOCATION/$TARGET_NAME/src" > /dev/null 2>&1
     echo "Successfully deployed project to the device"
 done
 echo "Deployment Complete!"
